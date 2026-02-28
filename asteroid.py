9dee7dad255608d0b272d1b2c3125ab0026877b7
Aarrence <fynn@purpledragonwear.com>
2026-02-27 17:35:12 -0800

    L: Made larger Asteroids split

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 import pygame
-from constants import LINE_WIDTH
+import random
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 from circleshape import CircleShape
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        else:
+            log_event("asteroid_split")
+            random_angle = random.uniform(20, 50)
+            asteroid1_angle = self.velocity.rotate(random_angle)
+            asteroid2_angle = self.velocity.rotate(-random_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            Asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+            Asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+            Asteroid_1.velocity = asteroid1_angle * 1.2
+            Asteroid_2.velocity = asteroid2_angle * 1.2

13e55babd91f1ca25e5d95139b42ad8a064b6822
Aarrence <fynn@purpledragonwear.com>
2026-02-27 17:08:58 -0800

    I: Added Shoot method to player character

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -20,6 +21,8 @@ def main():
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroidfield = AsteroidField()
+    shots = pygame.sprite.Group()
+    Shot.containers = (shots, drawable, updatable)
     while True:
         log_state()
         for event in pygame.event.get():
